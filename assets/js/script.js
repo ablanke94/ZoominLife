d2ee9f5b2d28f22232a7e9519cc1e5b00d0b6421
KnightNight <gilbenvenitz18@gmail.com>
2022-04-12 14:24:29 -0500

    finished a third of the weather api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,83 @@
 // Variables
-var city = "Austin";
+var city = 'Austin';
 var key = '7e0d1756ab93961fb340fd9cdc867eda';
 var coords = [];
+var weatherBtn = document.querySelector('.dropdown-trigger');
+// weather API variables
+var currTemp = document.getElementById('current-temp');
+var currWeather = document.getElementById('current-weather');
+var currWindSpeed = document.getElementById('current-wind-speed');
+var currSunrise = document.getElementById('current-sunrise');
+var currSunset = document.getElementById('current-sunset');
 
 // function DECLARATIONS
-
-function weatherApi(){
-    // let city = something else 
-    console.log(city);
-    
-    var apiUrl = 'https://api.openweathermap.org/geo/1.0/direct?q=' +city+ '&limit=5&appid=' + key;
-    fetch(apiUrl)
+// Weather API functions
+function weatherApi() {
+  var apiUrl =
+    'https://api.openweathermap.org/geo/1.0/direct?q=' +
+    city +
+    '&limit=5&units=imperial&appid=' +
+    key;
+  fetch(apiUrl)
     .then(function (response) {
-        return response.json();
+      return response.json();
     })
     .then(function (data) {
+      console.log(data);
+      let lat = data[0].lat;
+      let lon = data[0].lon;
 
-        console.log(data);
-        let lat = data[0].lat;
-        let lon = data[0].lon;
-        console.log(lat);
-        console.log(lon);
-        coords =[ lat, lon]
-        console.log(coords);
-        // function here
-    });    
+      // function here
+      dailyWeatherApi(lat, lon, key);
+    });
+}
 
-}    
+function dailyWeatherApi(lat, lon, key) {
+  var apiUrl =
+    'https://api.openweathermap.org/data/2.5/weather?lat=' +
+    lat +
+    '&lon=' +
+    lon +
+    '&units=imperial&appid=' +
+    key;
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        // console.log(response);
+        response.json().then(function (data) {
+          console.log(data);
+          var temp = Math.round(data.main.temp);
+          var weather = data.weather.main;
+          let windSpeed = data.wind.speed;
+          let sunrise = data.sys.sunrise;
+          let sunset = data.sys.sunset;
+          currentWeather(temp, weather, windSpeed, sunrise, sunset);
+        });
+      } else {
+        alert('Error: ' + response.statusText); // if response was not okay then its sending an Alert (which is bad bc its a blocker) with the response status displayed
+      }
+    })
+    .catch(function (error) {
+      alert('Unable to connect to weatherAPI'); //is there is a server side error then an Alert (which is bad bc its a blocker) with the response will be displayed
+    });
+}
 
+function currentWeather(temp, weather, windSpeed, sunrise, sunset) {
+  currTemp.textContent = 'Current Temperature: ' + temp + '°F';
+  currWeather.textContent = 'Current Weather: ' + weather;
+  currWindSpeed.textContent = 'Current Wind Speed: ' + windSpeed + 'miles/hour';
+  currSunrise.textContent = "Today's Sunrise: " + sunrise;
+  currSunset.textContent = "Today's Sunset: " + sunset;
+}
+
+function weatherIcon() {}
+
+// End of weather API's
+// function declaration END
 weatherApi();
-console.log(coords);
+
+// Weather api Collapsable menu initialization
+document.addEventListener('DOMContentLoaded', function () {
+  var elems = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(elems, {});
+});
